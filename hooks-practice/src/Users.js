ec8ccabdd97bc3033e131517bb929d6c62ec2b29
Brian Kim <briaansjkim@gmail.com>
2020-06-02 02:13:26 -0400

    imported link component to direct user to new page

--- hooks-practice/src/Users.js
+++ hooks-practice/src/Users.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = ({ allUsers }) => {
   return (
     <div>
       {allUsers.map((user, idx) =>
-        <div className="User" key={idx}>
-          <div className="User-name">
-            {user.name}
+        <Link key={idx} to={{
+          pathname: '/photos',
+          state: { user: user }
+        }}>
+          <div className="User">
+            <div className="User-name">
+              {user.name}
+            </div>
+            <div className="User-company">
+              {user.company.name}
+              <br />
+              {user.company.catchPhrase}
+            </div>
           </div>
-          <div className="User-company">
-            {user.company.name}
-            <br />
-            {user.company.catchPhrase}
-          </div>
-        </div>
-      )}
-    </div>
+        </Link>
+      )
+      }
+    </div >
   )
 }
 
