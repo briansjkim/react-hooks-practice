e78822ec7227a37e4372f8e839e0fdf4cb44d322
Brian Kim <briaansjkim@gmail.com>
2020-06-02 01:57:15 -0400

    made api call and retrieved data

--- hooks-practice/src/App.js
+++ hooks-practice/src/App.js
@@ -1,37 +1,91 @@
-import React, { useState } from 'react';
-// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Users from './Users.js';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [allUsers, setUsers] = useState([]);
+  const [searchedUser, setSearchedUser] = useState('');
+  const [isLoading, setLoading] = useState(true);
+  const [noResults, setNoResults] = useState(false);
 
+  const callAPI = async () => {
+    const users = await fetch('https://jsonplaceholder.typicode.com/users');
+    const fetchedUsers = await users.json();
+    return fetchedUsers;
+  }
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const resUsers = await callAPI();
+        setUsers(resUsers);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const handleChange = (val) => {
+    setSearchedUser(val);
+  }
+
+  const submitSearch = () => {
+    async function filterUser() {
+      try {
+        const res = await callAPI();
+        let filteredUsers = [];
+        for (let i = 0; i < res.length; i++) {
+          let users = res[i];
+          let usernames = users.name.toLowerCase();
+          if (usernames.includes(searchedUser.toLowerCase())) {
+            filteredUsers.push(users);
+          }
+        }
+
+        if (filteredUsers.length > 0) {
+          setUsers(filteredUsers);
+          setNoResults(false);
+        } else {
+          setNoResults(true);
+        }
+
+        setSearchedUser('');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    filterUser();
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  };
 
   return (
     <div className="App">
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Click Me!</button>
+      <header className="App-header">
+        <h1>Homepage</h1>
+        <div className="Search">
+          <input
+            className="Search-user"
+            type="text"
+            placeholder="Search User's Name"
+            value={searchedUser}
+            onChange={e => handleChange(e.target.value)}
+          />
+          <input
+            className="Submit-search"
+            type="submit"
+            value="Search"
+            onClick={submitSearch}
+          />
+        </div>
+        {noResults ? 'No Results' : <Users allUsers={allUsers} />}
+      </header>
     </div>
   )
 }
