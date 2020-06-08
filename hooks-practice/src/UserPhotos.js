277b099943b523ff17e32bc8f4264ec7cfcbd83c
Brian Kim <briaansjkim@gmail.com>
2020-06-08 14:58:35 -0400

    added pagination component

--- hooks-practice/src/UserPhotos.js
+++ hooks-practice/src/UserPhotos.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import AlbumImage from './AlbumImage.js';
 import { useHistory } from 'react-router-dom';
+import Pagination from './Pagination.js';
 
 const UserPhotos = () => {
   const [albums, setAlbums] = useState([]);
   const [photos, setPhotos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [picsPerPage] = useState(5);
   const [isLoading, setLoading] = useState(true);
   const history = useHistory();
   const userId = history.location.state.user.id;
@@ -31,6 +34,15 @@ const UserPhotos = () => {
     fetchData();
   }, [userId]);
 
+
+  // Getting current pics
+
+  const idxOfLastPic = currentPage * picsPerPage;
+  const idxOfFirstPic = idxOfLastPic - picsPerPage;
+  const currentPics = photos.slice(idxOfFirstPic, idxOfLastPic);
+  console.log(currentPics);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="User-photos">
       <div className="User-header">
@@ -39,10 +51,21 @@ const UserPhotos = () => {
       {isLoading ? (
         <div>Loading...</div>
       ) : (
+        <div>
           <div className="User-albums">
             {albums.map((album, idx) =>
-              <AlbumImage album={album} photo={photos[idx]} key={idx} />
+              <AlbumImage 
+                album={album} 
+                photo={currentPics[idx]} 
+                key={idx} 
+              />
             )}
+          </div>
+          <Pagination
+              picsPerPage={picsPerPage}
+              totalPics={photos.length}
+              paginat={paginate}
+          />
           </div>
         )}
     </div>
